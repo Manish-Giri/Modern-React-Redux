01236c67d8370c563236c605181dae2de58035fc
MANISH-GIRI <manish.giri.me@gmail.com>
2017-07-14 23:52:41 -0400

    Remove API Key from index.js

--- Section-1/ReduxSimpleStarter/src/index.js
+++ Section-1/ReduxSimpleStarter/src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import API_KEY from './api_key';
+let searchTerm = "apple";
 
-const API_KEY = "";
-const App = function() {
-  return (
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {videos: []}
+
+    YTSearch({key: API_KEY, term: searchTerm}, function(data) {
+      console.log(data);
+      this.setState({videos: data});
+    })
+  }
+
+  render() {
+    return (
     <div>
       <SearchBar/>
     </div>
-  )
+    )
+  }
 }
 
 ReactDOM.render(
