cb6f0dd4e3a26c348c6b2a5dc597e279baf982c5
MANISH-GIRI <manish.giri.me@gmail.com>
2017-07-15 23:58:14 -0400

    Select user's clicked video to be rendered as main video

--- Section-1/ReduxSimpleStarter/src/components/video_list_item.jsx
+++ Section-1/ReduxSimpleStarter/src/components/video_list_item.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const VideoListItem = props => {
     const video = props.video;
+    const onVideoSelect = props.onVideoSelect;
     const imageURL = props.video.snippet.thumbnails.default.url;
+
     return (
-        <li className="list-group-item">
+        <li className="list-group-item" onClick={() => onVideoSelect(video)}>
             <div className="video-list-media">
                 <div className="media-left">
                     <img src={imageURL} alt="" className="media-object"/>
