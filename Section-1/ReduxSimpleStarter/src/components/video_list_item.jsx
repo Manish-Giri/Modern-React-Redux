3f9a2eea9b547314624637c68c63b79b92d5becb
MANISH-GIRI <manish.giri.me@gmail.com>
2017-07-15 23:05:55 -0400

    Render basic list of videos with thumbnail and title from API response

--- Section-1/ReduxSimpleStarter/src/components/video_list_item.jsx
+++ Section-1/ReduxSimpleStarter/src/components/video_list_item.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 
 const VideoListItem = props => {
     const video = props.video;
+    const imageURL = props.video.snippet.thumbnails.default.url;
     return (
         <li className="list-group-item">
             <div className="video-list-media">
                 <div className="media-left">
-                    <div className="img">
-
-                    </div>
+                    <img src={imageURL} alt="" className="media-object"/>
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">
+                        {video.snippet.title}
+                    </div>    
                 </div>
             </div>
         </li>
-    
-   
+
     )
 };
 
